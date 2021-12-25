6fae52ceb130082b9435ef876fe0dd9ad03c4744
anhld <wolfwithcode@gmail.com>
2021-12-25 11:44:45 +0700

    add delete todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,25 +4,24 @@
     <ul>
       <li v-for="todo in allTodos" :key="todo.id" class="todo">
         {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import {mapGetters, mapActions} from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 export default {
-    name: 'Todos',
-    methods: {
-        ...mapActions(['fetchTodos'])
-    },
-    computed: 
-        mapGetters(['allTodos']),
-    created() {
-        this.fetchTodos();
-    }
-    
-}
+  name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo"]),
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  },
+};
 </script>
 
 <style scoped>
@@ -39,5 +38,38 @@ export default {
   text-align: center;
   position: relative;
   cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
